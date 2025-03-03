aee0d557b65f3ce50b51c2b77d891b2a5201eae4
Steffen Meinecke <steffen.meinecke@uni-kassel.de>
2025-03-03 17:19:25 +0100

    proceed power sytem test case reconversion

--- pandapower/networks/compare_power_system_test_case_jsons.py
+++ pandapower/networks/compare_power_system_test_case_jsons.py
@@ -2,7 +2,12 @@ import os
 import pandapower as pp
 from pandapower.networks.create_power_system_test_case_jsons import test_case_table_to_create
 import pathlib
+try:
+    import pandaplan.core.pplog as logging
+except ImportError:
+    import logging
 
+logger = logging.getLogger(__name__)
 home = str(pathlib.Path.home())
 
 
@@ -14,8 +19,14 @@ def compare_json_folders(folder1, folder2):
     folder1 = defaulting_folder_integers(folder1)
     folder2 = defaulting_folder_integers(folder2)
 
+    if not os.path.isdir(folder1):
+        raise FileNotFoundError(f"This path does not exist as directory: {folder1}")
+    if not os.path.isdir(folder2):
+        raise FileNotFoundError(f"This path does not exist as directory: {folder2}")
+
     test_case_df, _ = test_case_table_to_create()
     net_names = list(test_case_df.name)
+    net_names = ["case33bw", "case300", "case6470rte", "case6495rte", "case6515rte"] # TODO for testing
 
     eq = list()
     not_eq = list()
@@ -24,15 +35,32 @@ def compare_json_folders(folder1, folder2):
     for net_name in net_names:
         try:
             net1 = pp.from_json(os.path.join(folder1, f"{net_name}.json"))
-            net2 = pp.from_json(os.path.join(folder1, f"{net_name}.json"))
+            net2 = pp.from_json(os.path.join(folder2, f"{net_name}.json"))
         except:
             load_err.append(net_name)
         else:
+            if 1:
+                et_cts1 = pp.count_elements(net1)
+                et_cts2 = pp.count_elements(net2)
+                for et in et_cts1.index.union(et_cts2.index):
+                    cols_to_ignore = ["name"]
+                    if "et" == "load":
+                        cols_to_ignore.append("type")
+                    df_eq = pp.dataframes_equal(net1[et][net1[et].columns.difference({"name"})],
+                                                net2[et][net1[et].columns.difference({"name"})])
+                    print(f"{et}: {df_eq}")
             is_eq = pp.nets_equal(net1, net2, check_only_results=True)
             if is_eq:
                 eq.append(net_name)
             else:
                 not_eq.append(net_name)
+
+    if len(load_err):
+        logger.error(f"These networks could not be loaded from json files:\n{load_err}")
+    if len(not_eq):
+        logger.error(f"The comparison reveald result differences for these networks:\n{not_eq}")
+
+    return eq, not_eq, load_err
 
 
 def defaulting_folder_integers(folder):
@@ -41,4 +69,4 @@ def defaulting_folder_integers(folder):
 
 
 if __name__ == "__main__":
-    compare_json_folders(1, 2)
+    compare_json_folders(1, os.path.join(home, "desktop", "develop"))
--- pandapower/networks/create_power_system_test_case_jsons.py
+++ pandapower/networks/create_power_system_test_case_jsons.py
@@ -4,10 +4,11 @@ from io import StringIO
 import numpy as np
 import pandas as pd
 
-from pandapower.converter.matpower.from_mpc import _mpc2ppc
+from pandapower.converter.matpower.from_mpc import _m2ppc
 from pandapower.converter.pypower.from_ppc import from_ppc
 from pandapower.plotting.generic_geodata import create_generic_coordinates
 from pandapower.auxiliary import LoadflowNotConverged
+from pandapower.file_io import to_json
 from pandapower.run import runpp
 import pandapower.networks as pn
 try:
@@ -26,33 +27,39 @@ def create_power_system_test_case_jsons(output_folder=None):
     if output_folder is None:
         val = 1
         output_folder = os.path.join(home, "Desktop", f"power_system_test_case_jsons_{val}")
-        while os.path.exists(val):
+        while os.path.exists(output_folder) and not (
+                os.path.isdir(output_folder) and len(os.listdir((output_folder))) == 0):
             val += 1
             output_folder = os.path.join(home, "Desktop", f"power_system_test_case_jsons_{val}")
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
 
     test_case_df, input_data_folders = test_case_table_to_create()
 
     for row in test_case_df.itertuples():
-        logging.info(f"Start loading and converting {row.name}")
+        logger.info(f"Start loading and converting '{row.name}'")
 
         # --- load the net
         ppc = load_ppc(row.name, input_data_folders[row.folder_no])
 
+        ### TODO:
+        # line r & x sind anders
+
         # --- correct, convert and adjust the net
-        correct_or_adapt_input_data(ppc, net_name)
+        correct_or_adapt_input_data(ppc, row.name)
         net = from_ppc(ppc, f_hz=row.f_hz)
         try_to_take_bus_geo_data_from_existing(net, row.name)
         try_to_provide_pf_results(net, row.name)
 
         # --- store the net
-        pp.to_json(net, os.path.join(output_folder, row.name + '.json'))
+        to_json(net, os.path.join(output_folder, row.name + '.json'))
 
-    logging.info(f"Loading, converting, and saving of all power system test cases completed.")
+    logger.info(f"Loading, converting, and saving of all power system test cases completed.")
 
 
 def load_ppc(net_name, folder):
-    mat_case_path = os.path.join(folder, net_name + '.mat')
-    return _mpc2ppc(mat_case_path, casename_mpc_file='mpc')
+    m_case_path = os.path.join(folder, net_name + '.m')
+    return _m2ppc(m_case_path)
 
 
 def test_case_table_to_create():
@@ -73,7 +80,7 @@ case_ieee30;60;1
 case33bw;60;1
 case89pegase;50;1
 case145;60;1
-case_illinois200;60;1
+case_illinois200;60;2
 case1354pegase;50;1
 case1888rte;50;1
 case2848rte;50;1
@@ -88,10 +95,11 @@ GBreducednetwork;50;2
 iceland;50;2
 """
     input_data_folders = pd.Series({
-        1: os.path.join("Documents", "MATLAB", "matpower8.0"),
-        2: os.path.join("Documents", "further_mpc_data"),
+        1: os.path.join(home, "Documents", "MATLAB", "matpower8.0", "data"),
+        2: os.path.join(home, "Documents", "MATLAB", "further_mpc_data"),
     })
     table = pd.read_csv(StringIO(table_str), sep=";")
+    table = table.loc[table.name.isin(["case33bw"])]#, "case300", "case6470rte", "case6495rte", "case6515rte"])] # TODO for testing
     return table, input_data_folders
 
 
@@ -102,6 +110,19 @@ def correct_or_adapt_input_data(ppc, net_name):
     if net_name == "case14":
         # fix missing vn_kv values
         ppc["bus"][:, 9] = np.array([135]*5+[0.208, 14, 12]+[0.208]*6)
+
+    elif net_name == "case33bw":
+        # fix wrong unit of load powers
+        load_rows = ~np.isclose(ppc["bus"][:, 2], 0)
+        PL_max = np.max(ppc["bus"][load_rows, 2])
+        PL_min = np.min(ppc["bus"][load_rows, 2])
+        if PL_min > 0.5 and PL_max > 50:
+            logger.info("Powers in 'case33bw' are given in MW/Mvar, however, these are corrected "
+                        "to kW/kvar according to the original paper.")
+            ppc["bus"][:, [2, 3]] *= 1e-3
+        elif PL_min <= 0.5 and PL_max > 50:
+            raise ValueError("In 'case33bw', the power values of the loads are not as expected.")
+
     elif net_name == "case57":
         # fix missing vn_kv values
         ppc["bus"][:, 9] = np.array([115]*17+[500]*3+[138]*4+[345]+[161]*4+[345]*4+[138]*7 +
@@ -112,23 +133,30 @@ def correct_or_adapt_input_data(ppc, net_name):
         ppc["bus"][115, 9] = 345
     #    ppc["branch"][-4, 4] = 0  # made before june 2018
     #    ppc["branch"][133, 4] = 0  # made before june 2018
+
     elif net_name == "case1888rte":
-        _choose_slack_buses_with_connected_gens(ppc, 1267)
+        _choose_slack_buses_with_connected_gens(ppc, net_name, 1267)
+
     elif net_name == "case1951rte":
-        _choose_slack_buses_with_connected_gens(ppc, 179)
+        _choose_slack_buses_with_connected_gens(ppc, net_name, 179)
+
     elif net_name == "case2848rte":
-        _choose_slack_buses_with_connected_gens(ppc, 239)
+        _choose_slack_buses_with_connected_gens(ppc, net_name, 239)
+
     elif net_name == "case6468rte":
-        _choose_slack_buses_with_connected_gens(ppc, 6218)
+        _choose_slack_buses_with_connected_gens(ppc, net_name, 6218)
+
     elif net_name == "case6470rte":
-        _choose_slack_buses_with_connected_gens(ppc, 5988)
+        _choose_slack_buses_with_connected_gens(ppc, net_name, 5988)
+
     elif net_name == "case6495rte":
-        _choose_slack_buses_with_connected_gens(ppc, [6077, 6161, 6307, 6308, 6309, 6310])
+        _choose_slack_buses_with_connected_gens(ppc, net_name, [6077, 6161, 6307, 6308, 6309, 6310])
+
     elif net_name == "case6515rte":
-        _choose_slack_buses_with_connected_gens(ppc, 6171)
+        _choose_slack_buses_with_connected_gens(ppc, net_name, 6171)
 
 
-def _choose_slack_buses_with_connected_gens(ppc, new_slack_buses):
+def _choose_slack_buses_with_connected_gens(ppc, net_name, new_slack_buses):
     """In the original data of some power system test cases inappropriate buses are selected as
     reference buses, i.e. slack buses. For example, a bus without connected generator is a bad
     assumption and cannot be modeled by pandapower.
@@ -143,6 +171,8 @@ def _choose_slack_buses_with_connected_gens(ppc, new_slack_buses):
                            new_slack_buses]
     idx_new_slack_gens = [idx for idx in range(len(ppc["gen"])) if ppc["gen"][idx, 0] in
                           new_slack_buses]
+    assert len(new_slack_buses)  # new slack buses were found
+    assert len(idx_new_slack_gens)  # new slack buses have connected gens
 
     idx_original_slack_buses = np.where(ppc["bus"][:, 1] == 3)[0]
     name_original_slack_buses = ppc["bus"][idx_original_slack_buses, 0][0]
@@ -150,9 +180,9 @@ def _choose_slack_buses_with_connected_gens(ppc, new_slack_buses):
         name_original_slack_buses, '__iter__') else [int(name_original_slack_buses)]
     idx_original_slack_gens = [idx for idx in range(len(ppc["gen"])) if ppc["gen"][idx, 0] in
                                name_original_slack_buses]
-
-    assert len(idx_original_slack_gens) == 0  # there is no gen connected to original slack buses
-    assert len(idx_new_slack_gens) >= len(new_slack_buses)  # all new slack buses have connected gens
+    if len(idx_original_slack_gens):
+        logger.info(f"In {net_name}, the slack is changed although a generator was connected to "
+                    "the old slack bus.")
 
     # --- change original slack buses to new_slack_buses
     ppc["bus"][idx_original_slack_buses, 1] = 2
@@ -172,7 +202,7 @@ def try_to_provide_pf_results(net, net_name):
     try:
         runpp(net, trafo_model="pi")
     except (LoadflowNotConverged, KeyError):
-        logger.info(f"Power flow calculation of {row.name} did not converge.")
+        logger.error(f"Power flow calculation of {net_name} did not converge.")
 
 
 if __name__ == "__main__":
